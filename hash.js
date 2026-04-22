ce33be39aa13bd12b6c3ad724bf9cfd3e7fc5733
classuser324 <classuser324@local.iful.edu.ua>
2026-04-22 09:31:35 +0300

    password hasher project

---
+++ hash.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const bcrypt = require("bcrypt");
+
+const filePath = "password.txt";
+const testPassword = "bacon";
+
+async function run() {
+  try {
+    // перевіряємо, чи існує файл
+    if (!fs.existsSync(filePath)) {
+      console.log("Файл не знайдено. Створюємо новий хеш...");
+
+      const password = "bacon"; // початковий пароль
+      const saltRounds = 10;
+
+      const hash = await bcrypt.hash(password, saltRounds);
+
+      fs.writeFileSync(filePath, hash);
+      console.log("Пароль захешовано і збережено.");
+    } else {
+      console.log("Файл знайдено. Перевіряємо пароль...");
+
+      const savedHash = fs.readFileSync(filePath, "utf-8");
+
+      const isMatch = await bcrypt.compare(testPassword, savedHash);
+
+      if (isMatch) {
+        console.log("Password is correct");
+      } else {
+        console.log("Password is incorrect");
+      }
+    }
+  } catch (err) {
+    console.error("Помилка:", err);
+  }
+}
+
+run();
